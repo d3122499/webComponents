acefb88d3a2b1363deb7ce8897844fd5767c24ee
d3122499 <141051147+d3122499@users.noreply.github.com>
2023-11-17 10:33:29 -0300

    Update customScripts.js

--- Scripts/9002/customScripts.js
+++ Scripts/9002/customScripts.js
@@ -1,23 +1,96 @@
-function ShowProgress() {
+function checkAll(objRef) {
 
-                    var strVin = document.getElementById("<%=objTextbox_strVin.ClientID %>").value;
-            
-                    if (strVin == null || strVin == '')
-                        return;
+    var GridView = objRef.parentNode.parentNode.parentNode;
+    var inputList = GridView.getElementsByTagName("input");
+    var intMarkedRows = 0;
 
-                    setTimeout(function () {
-                        var modal = $('<div />');
-                        modal.addClass("modal");
-                        $('body').append(modal);
-                        var loading = $(".loading");
-                        loading.show();
-                        var top = Math.max($(window).height() / 2 - loading[0].offsetHeight / 2, 0);
-                        var left = Math.max($(window).width() / 2 - loading[0].offsetWidth / 2, 0);
-                        loading.css({ top: top, left: left });
-                    }, 200);
+    for (var i = 0; i < inputList.length; i++) {
+
+        //Get the Cell To find out ColumnIndex
+        var row = inputList[i].parentNode.parentNode;
+        if (inputList[i].type == "checkbox" && objRef != inputList[i]) {
+
+            if (objRef.checked) {
+                //If the header checkbox is checked
+                //check all checkboxes
+                //and highlight all rows
+                if (inputList[i].disabled == false) {
+                    intMarkedRows = intMarkedRows + 1;
+                    row.style.backgroundColor = "aqua";
+                    inputList[i].checked = true;
                 }
+            }
 
-                $('form').live("submit", function () {
-                ShowProgress();
+            else {
+                //If the header checkbox is checked
+                //uncheck all checkboxes
+                //and change rowcolor back to original
+                if (inputList[i].disabled == false) {
+                    if (row.rowIndex % 2 == 0) {
+                        //Alternating Row Color
+                        row.style.backgroundColor = "#C2D69B";
+                    }
+                    else {
+                        row.style.backgroundColor = "white";
+                    }
 
-                });
+                    inputList[i].checked = false;
+                }                        
+            }
+        }
+    }
+
+    //validateButtonsProcess(intMarkedRows);
+}
+
+function Check_Click(objRef) {
+
+    var intMarkedRows = 0;
+
+    //Get the Row based on checkbox
+    var row = objRef.parentNode.parentNode;
+
+    if (objRef.checked) {
+        //If checked change color to Aqua
+        row.style.backgroundColor = "aqua";
+    }
+    else {
+
+        //If not checked change back to original color
+        if (row.rowIndex % 2 == 0) {
+
+            //Alternating Row Color
+            row.style.backgroundColor = "#C2D69B";
+        }
+        else {
+            row.style.backgroundColor = "white";
+        }
+    }
+
+    //Get the reference of GridView
+    var GridView = row.parentNode;
+
+    //Get all input elements in Gridview
+    var inputList = GridView.getElementsByTagName("input");
+
+    for (var i = 0; i < inputList.length; i++) {
+
+        //The First element is the Header Checkbox
+        var headerCheckBox = inputList[0];
+
+        //Based on all or none checkboxes
+        //are checked check/uncheck Header Checkbox
+        var checked = true;
+
+        if (inputList[i].type == "checkbox" && inputList[i] != headerCheckBox) {
+            if (!inputList[i].checked) {
+                checked = false;
+                break;
+            } else {
+                intMarkedRows = intMarkedRows + 1;
+            }
+        }
+    }
+    //validateButtonsProcess(intMarkedRows);
+    headerCheckBox.checked = checked;
+}
